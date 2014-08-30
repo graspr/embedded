921bf44e60b813ae25ca2c59a55bd530004023e5
Anton de Winter <adewinter@gmail.com>
2014-08-30 17:39:42 +0000

    ADC data finally coming through \!

--- rpi/graspr.py
+++ rpi/graspr.py
@@ -2,8 +2,7 @@ import wiringpi2 as wpi
 import signal
 import sys
 import time
-
-
+import spi
 
 HIGH = 1
 LOW = 0
@@ -15,7 +14,7 @@ A1 = 7
 A2 = 5
 A3 = 4
 EN = 8
-MUX_PINS = [A0, A1, A2, A3]
+MUX_PINS = [A3, A2, A1, A0]
 
 CSB = 10
 MISO = 13
@@ -24,7 +23,7 @@ SCKL = 14
 
 SPI_CHANNEL = 0  #on RPI there are two SPI channels, 0 and 1
 
-SPI_CLOCK_SPEED = 2000000 #hz
+SPI_CLOCK_SPEED = int(1E6) #hz
 
 CHANNELS = {
     14: [1, 1, 0, 1],
@@ -45,12 +44,25 @@ def read(channel):
     Reads the value of a channel (on the mux) from the adc
     """
     switch_to_channel(channel)
-    val = 16777216 #send a 24 bit number
-
+    # val = '%s' % 0b100000000000000000000001 #send a 24 bit number
+    val = '\n'
+    v1 = 0
+    v2 = 0
+    wpi.digitalWrite(CSB, LOW)
+    # val = spi.transfer((0xff, 0xff, 0xff))
     retcode = wpi.wiringPiSPIDataRW(SPI_CHANNEL, val)
+    retcode = wpi.wiringPiSPIDataRW(SPI_CHANNEL, val)
+    # print val
+    v1 = val
+    retcode = wpi.wiringPiSPIDataRW(SPI_CHANNEL, val)
+    # print val
+    v2 = val
+    wpi.digitalWrite(CSB, HIGH)
     if retcode == -1:
-        raise Exception('ERROR PERFORMING SPI DATA RW on channel: %s, \
-                         retcode: %s' % (SPI_CHANNEL, retcode))
+        raise Exception('ERROR PERFORMING SPI DATA RW on channel: %s, retcode: %s' % 
+                        (SPI_CHANNEL, retcode))
+    # val = (v1 << 8) | v2
+    val = ''.join(format(ord(x), 'b').zfill(8) for x in [v1,v2])
     return val
 
 def run():
@@ -60,38 +72,42 @@ def run():
     setup()
     print 'Start of run: %s' % time.time()
     print 'Channel 14,Channel 15,Channel 16'
-    # print 'HELLO'
-    # foo = 0b000000000000000000000000
-    # wpi.wiringPiSPIDataRW(0, foo)
-    # print '.'
-    # print type(foo)
-    # print foo
-    # print '.'
 
     while True:
-        print '%s,' % read(14),
-        print '%s,' % read(15),
-        print '%s'  % read(16)
+        c15 = read(15)
+        # print '%s,' % read(14),
+        print '%s' % str(int(c15,2)).zfill(5)
+        # print '%s'  % read(16)
 
 def setup():
     """
     Perform initial board and SPI setup
     """
+    signal.signal(signal.SIGINT, signal_handler) #handler for keyboard interrupt
+
     wpi.wiringPiSetup()
 
     for pin in MUX_PINS:
         wpi.pinMode(pin, OUTPUT)
+
     wpi.pinMode(EN, OUTPUT)
     wpi.digitalWrite(EN, HIGH) #EN should always be high.
 
-    wpi.wiringPiSPISetup(0, SPI_CLOCK_SPEED)
+    wpi.pinMode(CSB, OUTPUT)
 
+    wpi.wiringPiSPISetup(0, SPI_CLOCK_SPEED)
+    # spi.openSPI(speed=1000000, mode=0)
+
+
+
+
+def signal_handler(signal, frame):
+    """
+    For when we hit CTRL+C!
+    """
+    print 'End of run: %s' % time.time()
+    sys.exit(0)
 
 if __name__ == "__main__":
     run()
-
-def signal_handler(signal, frame):
-    print 'End of run: %s' % time.time()
-    sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
 
